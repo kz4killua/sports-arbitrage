d1f0849a4208ef091755159fbab61876587f955c
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2023-06-01 20:47:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,10 @@ def main():
     # Create the data directory
     Path('data').mkdir(parents=True, exist_ok=True)
 
-    while True:
-
-        # Fetch data and save to JSON
-        try:
-            data = get()
-            with open(f'data/{int(time.time())}.json', 'w') as f:
-                json.dump(data, f)
-        except BaseException as e:
-            pass
-
-        # Wait 5 minutes
-        time.sleep(60 * 5)
+    # Fetch data and save to JSON
+    data = get()
+    with open(f'data/{int(time.time())}.json', 'w') as f:
+        json.dump(data, f)
 
 
 if __name__ == "__main__":
